2310a0816fb48e4eea6fea91f6cec2fd6212a0cb
clairechatbot69 <clairechatbot@gmail.com>
2024-09-09 23:11:40 +0800

    Update 00_00join.js

--- scripts/cmds/00_00join.js
+++ scripts/cmds/00_00join.js
@@ -6,7 +6,7 @@ module.exports = {
 	config: {
 		name: "join",
 		version: "2.0",
-		author: "Kshitiz",
+		author: "GeoBlo",
 		countDown: 5,
 		role: 0,
 		shortDescription: "Join the group that bot is in",
