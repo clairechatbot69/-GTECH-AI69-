5fc64263686c0b92103a96e63b831b5d08da713e
clairechatbot69 <clairechatbot@gmail.com>
2024-09-09 22:58:48 +0800

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
 	 name: "prefix",
 	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by cliff
+	 author: "George Nakila",//remodified by GeoBlo
 	 countDown: 5,
 	 role: 0,
 	 shortDescription: "no prefix",
@@ -15,11 +15,11 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 . 𓆪 ]\n
+Yo, my prefix is [ 𓆩 @ 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ ✓ʜᴇʟᴘ [number of page] -> sᴇᴇ ᴄᴏᴍᴍᴀɴᴅs
 ➥ ✓ᴀɪ [message] -> ᴛᴀʟᴋ ᴛᴏ ʙᴏᴛ/ᴀɪ
-➥ ✓ɪɴғᴏ [command] -> ɪɴғᴏʀᴍᴀᴛɪᴏɴ \n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/profile.php?id=100030880666720&mibextid=ZbWKwL`,
+➥ ✓ɪɴғᴏ [command] -> ɪɴғᴏʀᴍᴀᴛɪᴏɴ \n\nHave fun using it enjoy!❤️\nDeveloper: https://www.facebook.com/geotechph.net`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
